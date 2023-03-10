e4f28d1e492eff853c49f6143976d19293431e76
obasootom <onyearugbulem7@gmail.com>
2023-03-10 18:21:37 -0500

    adding html page

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use  std::net::TcpListener;
 use  std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
-    let listener: TcpListener  = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener  = TcpListener::bind("127.0.0.1:8080").expect("Oops can't secure a connection");
 
     for streamer in listener.incoming() {
         let stream = streamer.unwrap();
@@ -16,6 +17,20 @@ fn main() {
 fn Handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
     stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let content = fs::read_to_string("index.html").unwrap();
 
-    println!("Rquest {}", String::from_utf8_lossy(&buffer[..]))
+        let response = format!("HTTP/1.1 200 OK {}\r\n\r\n{}",content.len(),content);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let content = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("{}\n\r Content-Length {}\r\n\r\n{}",status_line,content.len(),content);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+    
 }
